e2877d8e10eb4158874a1fcb148ea6af9ea1addc
Robert Simpson <70786439+GT40-66@users.noreply.github.com>
2026-09-21 11:05:34 -0400

    Update main.py

--- Day1/main.py
+++ Day1/main.py
@@ -1,1 +1,5 @@
-
+print("Hello aspiring musician!")
+city = input("What city did you grow up in?\n")
+pet = input("What pet did you have growing up?\n")
+band_name = city + " " + pet
+print("Your band name is " + band_name)
